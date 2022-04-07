2a8db950035338fed16a3329409e77294b0f972e
mazen-alabasiiry <mazenabasiiry@gamil.com>
2022-04-07 18:25:46 +0200

    pagination added

--- src/context.js
+++ src/context.js
@@ -42,7 +42,7 @@ const AppProvider = ({ children }) => {
     try {
 
       const res = await fetch(
-        `https://www.omdbapi.com/?apikey=160e88be}&s=Batman&page=${currentPageIndex}`
+        `https://www.omdbapi.com/?apikey=160e88be&s=Batman&page=${currentPageIndex}`
       );
       const movies = await res.json();
       setMovies(movies.Search)
